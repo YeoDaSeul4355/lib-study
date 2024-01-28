5a6b83fdd51e8863a2a8272a0f6e6883bf125fc7
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-01-28 17:52:34 +0900

    💄 main 스타일링 수정

--- src/components/layout/Contents.jsx
+++ src/components/layout/Contents.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contents = ({ children }) => {
   return (
-    <main id="main" role="main">
+    <main id="main" className="space-y-40 mb-40" role="main">
       {children}
     </main>
   );
