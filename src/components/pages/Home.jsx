3e9542163a6add60c78c3ae9b16b75b012877d73
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-01-28 17:07:07 +0900

    💄 Header, Footer, Container 레이아웃 생성

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Contents from "../layout/Contents";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <Contents>
-        <Link to="/react-hook-form-login">
-          리액트 훅 폼으로 로그인 기능 구현하기
-        </Link>
-      </Contents>
+      <Contents></Contents>
     </>
   );
 };
--- src/components/pages/LoginForm/LoginForm.jsx
+++ src/components/pages/LoginForm/LoginForm.jsx
@@ -97,7 +97,7 @@ const LoginForm = () => {
               <button
                 type="submit"
                 disabled={isSubmitting}
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-white bg-primary hover:bg-primary_hover focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 로그인
               </button>
