69e664c1571282acfd153f837f2ec29d5aef5eca
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-01-28 17:52:10 +0900

    🎨 BannerSection 추가

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Contents from "../layout/Contents";
+import BannerSection from "../layout/section/BannerSection";
+
 // import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <Contents></Contents>
+      <BannerSection />
     </>
   );
 };
