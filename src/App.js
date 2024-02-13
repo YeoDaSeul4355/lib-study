30b436cd3c102bfbb28af04686b7803eb4c8aa33
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-02-13 17:03:14 +0900

    ✨ 로우터 연결

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./components/pages/Home";
-import LoginForm from "./components/pages/LoginForm/LoginForm";
+import LoginForm from "./components/pages/react-hook-form/LoginForm";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Library from "./components/pages/Library";
+import Project from "./components/pages/Project";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/library" element={<Library />}></Route>
+        <Route path="/project" element={<Project />}></Route>
         <Route path="/react-hook-form-login" element={<LoginForm />}></Route>
       </Routes>
       <Footer />
