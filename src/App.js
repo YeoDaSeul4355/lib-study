017ff041133a9aed9c53891403b614ca59877559
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-02-19 15:10:58 +0900

    🎨 로우터 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import LoginForm from "./pages/react-hook-form/LoginForm";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Library from "./pages/Library";
 import Project from "./pages/Project";
+import LoginForm from "./pages/react-hook-form/LoginForm";
+import ZustandMemo from "./pages/zustand-memo/ZustandMemo";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/library" element={<Library />}></Route>
         <Route path="/project" element={<Project />}></Route>
         <Route path="/react-hook-form-login" element={<LoginForm />}></Route>
+        <Route path="/zustand-memo" element={<ZustandMemo />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
