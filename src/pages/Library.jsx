257efb5c4a198ae8c30b53eb84f79b2ac634a539
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-03-02 14:57:47 +0900

    💄 스타일 수정 및 구조 변경

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -21,12 +21,12 @@ const Library = () => {
             >
               <img
                 src="./images/logo/react_hook_form.svg"
-                className="h-20 w-20 ml-6 mb-3"
+                className="h-20 w-20 ml-6 mb-3 hover:scale-110 transition-all duration-300"
                 alt="client logo"
               />
             </Link>
             <p>react-hook-form</p>
-            <Link to="/library/react-hook-form" target="_blank">
+            <Link to="/library/react-hook-form">
               <p className="mt-2 bg-primary py-1 text-center rounded-3xl text-white transition-all duration-500 hover:bg-primary_hover">
                 학습 내용
               </p>
