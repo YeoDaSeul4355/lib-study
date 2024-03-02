2010904bed8556facfda9d8dc92eae36d03e19f1
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-03-02 16:54:15 +0900

    ➕ 로우터 연결

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Project from "./pages/Project";
 import LoginForm from "./pages/react-hook-form/LoginForm";
 import ZustandMemo from "./pages/zustand-memo/ZustandMemo";
 import ReactHookForm from "./pages/library/ReactHookForm";
+import ZustandTodo from "./pages/zustand-todo/ZustandTodo";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/project" element={<Project />}></Route>
         <Route path="/react-hook-form-login" element={<LoginForm />}></Route>
         <Route path="/zustand-memo" element={<ZustandMemo />}></Route>
+        <Route path="/zustand-todo" element={<ZustandTodo />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
