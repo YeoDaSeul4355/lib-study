4a0d0827cd6bcd4f24e14cb0ecf65c9e00e51f5b
Yeo Da Seul <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-03-28 15:47:23 +0900

    📝yup 작성
    이미지는 변경해줘야함

--- src/pages/library/Yup.jsx
+++ src/pages/library/Yup.jsx
@@ -63,8 +63,7 @@ if (Object.keys(errors).length === 0) {
 } else {
   console.error(errors);
 }
-`}
-          ></CodeBlock>
+`} />
           <p>
             상태 관리 없이 구현한다면, 각 할 일 항목에 우선순위를 추가하는 것만으로도 <span className="text-primary">복잡성이 증가한다.</span><br />
             우선순위가 변경될 때마다 각 항목을 <span className="text-primary">다시 렌더링해야 하며, 우선순위 변경 로직도 추가</span>해야 한다.
@@ -95,8 +94,7 @@ const formData = {
 schema.validate(formData)
   .then(valid => console.log('유효한 데이터입니다.'))
   .catch(errors => console.error(errors));
-`}
-          ></CodeBlock>
+`}/>
 
           <h2 className="text-2xl mb-3 mt-10">📝yup 사용법</h2>
         
@@ -118,7 +116,7 @@ const schema = yup.object().shape({
   email: yup.string().email('유효한 이메일을 입력하세요.').required('이메일을 입력하세요.'),
 });
           
-`}></CodeBlock>
+`} />
           
           <h3 className="text-xl mb-3 mt-10">3. 데이터 유효성 검사</h3>
           <p>  
@@ -133,7 +131,7 @@ const schema = yup.object().shape({
 schema.validate(data)
   .then(valid => console.log('유효한 데이터입니다.'))
   .catch(errors => console.error(errors));
-`}></CodeBlock>
+`} />
 
            <h2 className="text-2xl mb-3 mt-10">💜schema 예시</h2>
            <p>
@@ -147,6 +145,78 @@ const schema = yup.string().required('문자열을 입력하세요.');
 
 schema.validate('hello')
   .then(valid => console.log('유효한 문자열입니다.'))
+  .catch(errors => console.error(errors));
+`} />
+
+          <h3 className="text-xl mb-3 mt-10">2. 숫자(number) 유효성 검사</h3>
+          <CodeBlock code={`import * as yup from 'yup';
+
+const schema = yup.number().positive().integer().required('양의 정수를 입력하세요.');
+
+schema.validate(42)
+  .then(valid => console.log('유효한 숫자입니다.'))
+  .catch(errors => console.error(errors));
+`} />
+
+          <h3 className="text-xl mb-3 mt-10">3. 부울(boolean) 유효성 검사</h3>
+          <CodeBlock code={`import * as yup from 'yup';
+
+const schema = yup.boolean().required('부울 값을 입력하세요.');
+
+schema.validate(true)
+  .then(valid => console.log('유효한 부울 값입니다.'))
+  .catch(errors => console.error(errors));
+`} />
+
+           <h3 className="text-xl mb-3 mt-10">4. 객체(object) 유효성 검사</h3>
+           <CodeBlock code={`import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  name: yup.string().required('이름을 입력하세요.'),
+  age: yup.number().positive().integer().required('나이를 입력하세요.'),
+});
+
+const data = {
+  name: 'John',
+  age: 30,
+};
+
+schema.validate(data)
+  .then(valid => console.log('유효한 객체입니다.'))
+  .catch(errors => console.error(errors));
+`} />
+          <h3 className="text-xl mb-3 mt-10">4. 배열(array) 유효성 검사</h3>
+          <CodeBlock code={`import * as yup from 'yup';
+
+const schema = yup.array().of(yup.number().required()).required('숫자 배열을 입력하세요.');
+
+const data = [1, 2, 3];
+
+schema.validate(data)
+  .then(valid => console.log('유효한 배열입니다.'))
+  .catch(errors => console.error(errors));
+`} />
+          <h3 className="text-xl mb-3 mt-10">5. 날짜(date) 유효성 검사</h3>
+          <CodeBlock code={`import * as yup from 'yup';
+
+const schema = yup.date().max(new Date(), '미래의 날짜를 입력하세요.').required('날짜를 입력하세요.');
+
+const date = new Date();
+
+schema.validate(date)
+  .then(valid => console.log('유효한 날짜입니다.'))
+  .catch(errors => console.error(errors));
+`} />
+
+          <h3 className="text-xl mb-3 mt-10">6. 정규표현식 사용하기</h3>
+          <CodeBlock code={`import * as yup from 'yup';
+
+const schema = yup.string().matches(/^\d{4}-\d{2}-\d{2}$/, '날짜 형식(YYYY-MM-DD)으로 입력하세요.').required('날짜를 입력하세요.');
+
+const dateString = '2024-03-30';
+
+schema.validate(dateString)
+  .then(valid => console.log('유효한 날짜입니다.'))
   .catch(errors => console.error(errors));
 `} />
         </div>
